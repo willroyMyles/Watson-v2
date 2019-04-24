7f72442d9d29a9d5a0704edbd1d859a1da2597f4
wll <willroymyles@yahoo.com>
2019-04-23 23:54:15 -0700

    Changes request header for getting eacxh specialization

--- models/Specializations.js
+++ models/Specializations.js
@@ -12,15 +12,19 @@ class Specializations {
     getInfo(specialization, callabck) {
         var request = require('request');
         var cheerio = require('cheerio');
-        request('https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors?orderBy=MajorName&search=' + specialization + '', (err, res, body) => {
+        const options = {
+            method: 'GET',
+            url: 'https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors?orderBy=MajorName&search=' + specialization + '',
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
+            }
+        }
+        request(options, (err, res, body) => {
             var $ = cheerio.load(body);
             var tableBody = $('tbody');
             var tableRow = tableBody.find('tr');
-
-
             var counter = 0;
             var array = [];
-
             tableRow.each(function(i, elem) {
                 if (counter == 2 || counter == 4 || counter == 5 || counter == 6) array.push($(this).children().children().text());
                 counter++;
